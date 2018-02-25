a8092073dfd95cd357f5210f240805788d850772
kamaleshpatil <kamalesh.patil@gmail.com>
2018-02-25 14:44:26 -0500

    natural scrolling - support for vert and horiz scrolling

--- scripts/synclient_enable_natural_scrolling.sh
+++ scripts/synclient_enable_natural_scrolling.sh
@@ -1,20 +1,37 @@
 #!/bin/bash
 
+alter_scroll_value() {
+   # which dimension are we updating
+   DimScrollDelta="$1"
+
+   # Dimensional natural scrolling
+   # ----------------------------
+   dim_scroll_delta=`synclient | grep $DimScrollDelta`
+
+   dim_scroll_delta_value_count=`echo $dim_scroll_delta | grep -oP '[0-9]+' | wc -l`
+
+   # proceed only if there is single value
+   if [[ 1 -eq "$dim_scroll_delta_value_count" ]]; then
+      dim_scroll_delta_value=`echo $dim_scroll_delta | grep -oP '[0-9]+'`
+
+      # use negation of that value as new value
+      #echo Setting new value to "$DimScrollDelta=-$dim_scroll_delta_value"
+      synclient "$DimScrollDelta=-$dim_scroll_delta_value"
+   fi
+}
+
 # check if synclient is installed
 which synclient &> /dev/null
 synclient_exists="$?"
 
 if [[ 0 -eq "$synclient_exists" ]]; then
-   vert_scroll_delta=`synclient | grep VertScrollDelta`
 
-   vert_scroll_delta_value_count=`echo $vert_scroll_delta | grep -oP '[0-9]+' | wc -l`
+   # Vertical natural scrolling
+   # --------------------------
+   alter_scroll_value VertScrollDelta
 
-   # proceed only if there is single value
-   if [[ 1 -eq "$vert_scroll_delta_value_count" ]]; then
-      vert_scroll_delta_value=`echo $vert_scroll_delta | grep -oP '[0-9]+'`
+   # Horizontal natural scrolling
+   # ----------------------------
+   alter_scroll_value HorizScrollDelta
 
-      # use negation of that value as new value
-      #echo Setting new value to "VertScrollDelta=-$vert_scroll_delta_value"
-      synclient "VertScrollDelta=-$vert_scroll_delta_value"
-   fi
 fi
